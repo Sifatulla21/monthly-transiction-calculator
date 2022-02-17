cb5e840bd62ed06acc43ca4ed26c470c99460c02
Sifatulla21 <sifat2109@gmail.com>
2022-02-17 14:20:31 +0600

    calc part done

--- calc.js
+++ calc.js
@@ -33,4 +33,55 @@ document.getElementById('calculate').addEventListener('click', function(){
         errorBalance.style.display = 'block';
         invalidBalance.style.display = 'none';
     }
+ });
+
+ //saving button click
+document.getElementById('saving-button').addEventListener('click', function(){
+//get value from income field    
+    const incomeField = document.getElementById('income');
+    const incomeSaving = incomeField.value;
+//get value from balance    
+    const balanceText = document.getElementById('balance');
+    const balanceTextSaving = balanceText.innerText;
+//saving value
+    const savingField =  document.getElementById('saving-field');
+    const saving = savingField.value;
+//calculate saving amount
+    const totalSavingAmount = (parseFloat(incomeSaving) / 100) * parseFloat(saving);
+//show saving amount
+    const savingAmountShow = document.getElementById('saving-amount');
+    savingAmountShow.innerText = totalSavingAmount;
+//calculate remaining balance
+    const remainingBalance = parseFloat(balanceTextSaving) - parseFloat(totalSavingAmount);
+//Show remaining balance
+    const remainingBalanceShow = document.getElementById('remaining-balance');
+    remainingBalanceShow.innerText = remainingBalance;
+//validate saving and remaining balance
+    const invalidtotalSavingAmount = document.getElementById('invalid-remaining-balance');
+    const invalidbalanceTextSaving = document.getElementById('remaining-amount-invalid');
+    if(remainingBalance < 0){
+        invalidtotalSavingAmount.style.display = 'block';
+        invalidbalanceTextSaving.style.display = 'none';
+    }
+    else{
+        invalidtotalSavingAmount.style.display = 'none';
+        invalidbalanceTextSaving.style.display = 'block';
+    }
+//validate saving input
+    const invalidSaving = document.getElementById('invalid-saving');
+    const savingAmountInvalid = document.getElementById('saving-amount-invalid');
+    if(saving > 0){
+        savingAmountInvalid.style.display = 'block';
+        invalidSaving.style.display = 'none';
+    }
+    else if(saving < 0){
+        savingAmountInvalid.style.display = 'none';
+        invalidSaving.style.display = 'block';
+    }
+    else if(typeof saving == 'string'){
+        savingAmountInvalid.style.display = 'none';
+        invalidSaving.style.display = 'block';
+    }
+
+
  })
